4e38acf517f648d048ffad1c669a31fc2979e5c8
sambi <birnbaum.sam@gmail.com>
2023-12-02 11:28:01 +0100

    Day2 cleanup

--- src/adventofcode2023/day02.py
+++ src/adventofcode2023/day02.py
@@ -1,36 +1,23 @@
 import re
 from functools import reduce
-from typing import Tuple, Iterable, Mapping
-from itertools import chain
+from typing import Iterable, Mapping
 import sys
 from operator import mul
 
 
-def parse_game(game: str) -> Tuple[int, Iterable[Iterable[Tuple[str, int]]]]:
-    game_id, rounds = re.match(r'Game (\d+): (.*)$', game).groups()
-    return int(game_id), (
-        (
-            (color, int(qty)) for qty, color in
-            (cube.strip().split(' ') for cube in round_.strip().split(', '))
-        )
-        for round_ in rounds.split('; ')
-    )
-
-
 def part1(input_: Iterable[str], constraints: Mapping[str, int]) -> int:
     return sum(
-        id_ for id_, rounds in (parse_game(row) for row in input_)
-        if all(qty <= constraints.get(color, 0) for color, qty in chain.from_iterable(rounds))
+        int(game.split(' ')[1]) for game, rounds in (row.split(': ') for row in input_)
+        if all(int(qty) <= constraints.get(color, 0) for qty, color in re.findall(r'(\d+) (\w+)', rounds))
     )
 
 
 def part2(input_: Iterable[str]) -> int:
-    def get_power(game: str) -> int:
-        n_cubes = {}
-        for qty, color in re.findall(r'(\d+) (\w+)', game):
-            n_cubes[color] = max(int(qty), n_cubes.get(color, 0))
-        return reduce(mul, n_cubes.values())
-    return sum(get_power(game) for game in input_)
+    return sum(reduce(mul, (reduce(
+        (lambda acc_max, val_color: {**acc_max, val_color[1]: max(int(val_color[0]), acc_max.get(val_color[1], 0))}),
+        re.findall(r'(\d+) (\w+)', game),
+        dict()
+    ).values())) for game in input_)
 
 
 if __name__ == '__main__':
