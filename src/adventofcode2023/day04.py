bef0fd0951d8b5116b9f0888cc428b3f16268a92
sambi <birnbaum.sam@gmail.com>
2023-12-04 09:23:10 +0100

    Day1 part1

--- src/adventofcode2023/day04.py
+++ src/adventofcode2023/day04.py
@@ -5,8 +5,7 @@ import re
 
 def part1(input_: Iterable[str]) -> int:
     return sum(
-        2 ** (hits - 1)
-        for row in input_
+        2 ** (hits - 1) for row in input_
         if (hits := (lambda winners, have: sum(n in set(winners) for n in have))(
             *(re.findall(r'(\d+)', group) for group in re.match(r'Card +\d+:([\d ]+)\|([\d ]+)', row).groups())
         ))
