eb2ee794be685d52fc13e784771deff7ab4905b3
Sam Birnbaum <sam.birnbaum@booking.com>
2023-12-06 16:27:22 +0100

    Day5

--- src/adventofcode2023/day05.py
+++ src/adventofcode2023/day05.py
@@ -1,67 +1,78 @@
-from itertools import groupby, islice
-from typing import Iterable, Tuple, Sequence
+import sys
+from functools import reduce
+from itertools import groupby, islice, batched, permutations, chain
+from typing import Iterable, Tuple, Sequence, Iterator, Optional
 import re
 
 
-def apply_mappings(mappings: Iterable[Tuple[int, int, int]], input_: int) -> int:
-    return next((
-        dest + delta for dest, source, len_ in mappings if 0 <= (delta := input_ - source) < len_
-    ), input_)
+def apply_mappings(values: Sequence[int], mappings: Sequence[Tuple[int, int]]) -> Sequence[int]:
+    return tuple(next((
+        dest + delta for dest, source, len_ in mappings if 0 <= (delta := value - source) < len_
+    ), value) for value in values)
 
 
-def parse_input(input_: Iterable[str]) -> Tuple[Iterable[int], Iterable[Iterable[Tuple[int, int, int]]]]:
-    iter_input = iter(input_)
-    seeds = (int(seed) for seed in re.findall(r'(\d+)', next(iter_input)))
+def parse_input(input_: Iterator[str]) -> Tuple[Iterable[Iterable[Tuple[int, int, int]]], Iterable[int]]:
+    seeds = (int(seed) for seed in re.findall(r'(\d+)', next(input_)))
     mappings = (
         tuple(
-            tuple(int(term) for term in re.findall(r'(\d+)', row))
+            tuple(int(term) for term in re.findall(r'(\d+)', row))[:3]
             for row in islice(map_group[1], 1, None)
         )
-        for map_group in groupby((row.strip() for row in iter_input), lambda x: bool(x)) if map_group[0]
+        for map_group in groupby((row.strip() for row in input_), lambda x: bool(x)) if map_group[0]
     )
-    return seeds, mappings
+    return (mappings, seeds)
+
 
 def part1(input_: Iterable[str]) -> int:
-    items, mappings = parse_input(input_)
+    return min(reduce(apply_mappings, *parse_input(iter(input_))))
 
-    for mapping_group in mappings:
-        items = (apply_mappings(mapping_group, item) for item in items)
-    return min(items)
+
+def part2(input_: Iterable[str]) -> int:
+
+    Interval = Tuple[int, int]
+
+    def shift(interval: Optional[Interval], by: int) -> Optional[Interval]:
+        if interval is None:
+            return None
+        return interval[0] + by, interval[1] + by
+
+    def intersect(interval1: Interval, interval2: Interval) -> Optional[Interval]:
+        if interval1[1] <= interval2[0] or interval2[1] <= interval1[0]:
+            return None
+        else:
+            return max(interval1[0], interval2[0]), min(interval1[1], interval2[1])
+
+    def subtract_subinterval(interval: Interval, subinterval: Interval) -> Iterator[Interval]:
+        if subinterval is None:
+            yield interval
+            return
+        if interval[0] < subinterval[0]:
+            yield interval[0], subinterval[0]
+        if subinterval[1] < interval[1]:
+            yield subinterval[1], interval[1]
+
+    mappings, seeds = parse_input(iter(input_))
+    seed_intervals = ((start, start + len_) for start, len_ in batched(seeds, 2))
+    mapping_intervals = (
+        (((source, source + len_), target - source) for target, source, len_ in mapping_group)
+        for mapping_group in mappings
+    )
+
+    intervals = seed_intervals
+    for mapping_block in mapping_intervals:
+        unmapped, mapped = intervals, tuple()
+        for map_source, offset in mapping_block:
+            for interval in unmapped:
+                intersection = intersect(interval, map_source)
+                unmapped = (*islice(unmapped, 1, None), *subtract_subinterval(interval, intersection))
+                if intersection:
+                    mapped = (*mapped, shift(intersection, offset))
+        intervals = chain(unmapped, mapped)
+
+    return min(intervals)[0]
+
 
 if __name__ == '__main__':
-    input_ = '''seeds: 79 14 55 13
-
-seed-to-soil map:
-50 98 2
-52 50 48
-
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
-
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
-
-water-to-light map:
-88 18 7
-18 25 70
-
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
-
-temperature-to-humidity map:
-0 69 1
-1 0 69
-
-humidity-to-location map:
-60 56 37
-56 93 4'''.split('\n')
-    print(parse_input(input_))
-
-
+    input_ = tuple(sys.stdin)
+    print(part1(input_))
+    print(part2(input_))
