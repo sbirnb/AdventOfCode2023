0a0f59e5525f276ccf588e8c2b045fdd7e2ff402
sambi <birnbaum.sam@gmail.com>
2023-12-06 19:22:04 +0100

    Day 5 cleanup

--- src/adventofcode2023/day05.py
+++ src/adventofcode2023/day05.py
@@ -1,17 +1,11 @@
 import sys
 from functools import reduce
-from itertools import groupby, islice, batched, permutations, chain
-from typing import Iterable, Tuple, Sequence, Iterator, Optional
+from itertools import groupby, islice, batched, chain
+from typing import Iterable, Tuple, Sequence, Iterator
 import re
 
 
-def apply_mappings(values: Sequence[int], mappings: Sequence[Tuple[int, int]]) -> Sequence[int]:
-    return tuple(next((
-        dest + delta for dest, source, len_ in mappings if 0 <= (delta := value - source) < len_
-    ), value) for value in values)
-
-
-def parse_input(input_: Iterator[str]) -> Tuple[Iterable[Iterable[Tuple[int, int, int]]], Iterable[int]]:
+def parse_input(input_: Iterator[str]) -> Tuple[Iterable[Tuple[Tuple[int, int, int]]], Iterable[int]]:
     seeds = (int(seed) for seed in re.findall(r'(\d+)', next(input_)))
     mappings = (
         tuple(
@@ -20,30 +14,27 @@ def parse_input(input_: Iterator[str]) -> Tuple[Iterable[Iterable[Tuple[int, int
         )
         for map_group in groupby((row.strip() for row in input_), lambda x: bool(x)) if map_group[0]
     )
-    return (mappings, seeds)
+    return mappings, seeds
 
 
 def part1(input_: Iterable[str]) -> int:
-    return min(reduce(apply_mappings, *parse_input(iter(input_))))
+    return min(reduce(
+        lambda values, mappings: tuple(next((
+            dest + delta for dest, source, len_ in mappings if 0 <= (delta := value - source) < len_
+        ), value) for value in values),
+        *parse_input(iter(input_))
+    ))
 
 
 def part2(input_: Iterable[str]) -> int:
 
-    Interval = Tuple[int, int]
-
-    def shift(interval: Optional[Interval], by: int) -> Optional[Interval]:
-        if interval is None:
-            return None
-        return interval[0] + by, interval[1] + by
-
-    def intersect(interval1: Interval, interval2: Interval) -> Optional[Interval]:
+    def intersect(interval1, interval2):
         if interval1[1] <= interval2[0] or interval2[1] <= interval1[0]:
-            return None
-        else:
-            return max(interval1[0], interval2[0]), min(interval1[1], interval2[1])
+            return 0, 0
+        return max(interval1[0], interval2[0]), min(interval1[1], interval2[1])
 
-    def subtract_subinterval(interval: Interval, subinterval: Interval) -> Iterator[Interval]:
-        if subinterval is None:
+    def subtract_subinterval(interval, subinterval):
+        if subinterval[0] == subinterval[1]:
             yield interval
             return
         if interval[0] < subinterval[0]:
@@ -51,25 +42,22 @@ def part2(input_: Iterable[str]) -> int:
         if subinterval[1] < interval[1]:
             yield subinterval[1], interval[1]
 
+    def apply_mapping(intervals, mapping):
+        new_unmapped, new_mapped = zip(*((
+            subtract_subinterval(interval, (intersection := intersect(interval, mapping[0]))),
+            tuple(end + mapping[1] for end in intersection)
+        ) for interval in intervals[0]))
+        return chain.from_iterable(new_unmapped),\
+            chain(intervals[1], filter(lambda interval: interval[0] != interval[1], new_mapped))
+
     mappings, seeds = parse_input(iter(input_))
-    seed_intervals = ((start, start + len_) for start, len_ in batched(seeds, 2))
-    mapping_intervals = (
-        (((source, source + len_), target - source) for target, source, len_ in mapping_group)
-        for mapping_group in mappings
-    )
 
-    intervals = seed_intervals
-    for mapping_block in mapping_intervals:
-        unmapped, mapped = intervals, tuple()
-        for map_source, offset in mapping_block:
-            for interval in unmapped:
-                intersection = intersect(interval, map_source)
-                unmapped = (*islice(unmapped, 1, None), *subtract_subinterval(interval, intersection))
-                if intersection:
-                    mapped = (*mapped, shift(intersection, offset))
-        intervals = chain(unmapped, mapped)
-
-    return min(intervals)[0]
+    return min(reduce(
+        lambda intervals, mappings: chain.from_iterable(reduce(apply_mapping, mappings, (intervals, tuple()))), (
+            (((source, source + len_), target - source) for target, source, len_ in mapping_group)
+            for mapping_group in mappings
+        ), ((start, start + len_) for start, len_ in batched(seeds, 2))
+    ))[0]
 
 
 if __name__ == '__main__':
