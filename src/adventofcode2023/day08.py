ef735dcb108501e5bad4b5ac144145b06b5f790a
Sam Birnbaum <sam.birnbaum@booking.com>
2023-12-08 10:55:42 +0100

    Day8 part2

--- src/adventofcode2023/day08.py
+++ src/adventofcode2023/day08.py
@@ -1,31 +1,35 @@
 import sys
 from itertools import accumulate, takewhile, islice, cycle
-from typing import Iterable, Tuple, Iterator
+from math import lcm
+from typing import Iterable, Tuple, Iterator, Mapping
 import re
 
 DIR_INDEX = {'L': 0, 'R': 1}
 
-def parse_input(input_: Iterator[str]) -> Tuple[str, Iterable[Tuple[str, Tuple[str, str]]]]:
-    return next(input_).strip(), dict(
-        (source, (left, right)) for source, left, right in
-        (re.findall(r'([A-Z]+)', row) for row in islice(input_, 1, None))
-    )
+
+def parse_input(input_: Iterator[str]) -> Tuple[str, Mapping[str, Tuple[str, str]]]:
+    return next(input_).strip(), {source: (left, right) for source, left, right in (
+        re.findall(r'(\w+)', row) for row in islice(input_, 1, None)
+    )}
 
 
 def part1(input_: Iterable[str]) -> int:
     moves, adjs = parse_input(iter(input_))
-    return sum(1 for _ in takewhile(
-        'ZZZ'.__ne__,
+    return sum(1 for _ in takewhile('ZZZ'.__ne__,
         accumulate(cycle(DIR_INDEX[dir_] for dir_ in moves), lambda pos, dir_: adjs[pos][dir_], initial='AAA')
     ))
 
+
 def part2(input_: Iterable[str]) -> int:
     moves, adjs = parse_input(iter(input_))
-    return sum(
-
-    )
+    return lcm(*(
+        sum(1 for _ in takewhile(lambda position: not position.endswith('Z'),
+            accumulate(cycle(DIR_INDEX[dir_] for dir_ in moves), lambda pos, dir_: adjs[pos][dir_], initial=initial)
+        )) for initial in adjs if initial.endswith('A')
+    ))
 
 
 if __name__ == '__main__':
     input_ = tuple(sys.stdin)
     print(part1(input_))
+    print(part2(input_))
